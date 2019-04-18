fba978eab5ae56bb3c25bc9f97fc86a20a295b81
Yasin Khatami <yasink@apple.com>
2019-04-18 14:06:08 -0700

    Adding helper files 1st time

---
+++ image_generator_with_roll.py
@@ -0,0 +1,61 @@
+from sklearn.utils import shuffle as sk_shuffle
+import numpy as np
+
+# Image data generator. Applies a random horizontal and vertical shift. It rolls the image, so pixels leaving from end, roll back to the begining of image.
+# Keras has a built in generator called ImageDataGenerator, which takes the arguments width_shift_range and height_shift_range to shift the image. When image is shifted to the right, the left pixels are repeated or set to a constant value etc.
+# This function rolls back the end of the image to the beginning and vice versa. So we don't need to repeat the pixels.  
+# This is not written really to be used for images. This is written for signals that can be rotated. If you rotate an image horizontally, the cat is gonna look weird.
+# But if the data is from measurements of linear acceleration, then this will make sense. 
+""" 
+Why use this? This is similar to vertical/horizontal flipping of an image but it is more general because it 
+rolls the data by applying random shift. This is good then the point where we start measurement does not matter. So we can
+shift the data in any direction. This helps us in generalizing the model better.
+Note that normalization is not supported here. It should be implemented before this step.
+"""
+def image_generator_ysn(x, y=None, batch_size=64, shuffle=True, enable_shift=True):
+    """
+    Arguments:
+        x: input image of size (n_x, n_h, n_w, n_ch)
+        y: second image of same size as x. Use only if the same operation is to be performed on another image y. Most likely should be None.
+        batch_size: size of batches of the generator
+        shuffle: enable or disable shuffling of data in the beginneing.
+        enable_shift: enable or disable vertical/horizontal shift. If disabled, this acts as a simple generator with batch_size.
+    """
+    if shuffle:
+        if y is not None:
+            x, y = sk_shuffle(x, y)
+        else:
+            x = sk_shuffle(x)
+    n_x = len(x)
+    n_h = x.shape[1]
+    n_w = x.shape[2]
+    i = 0
+    
+    # Generator loop
+    while True:
+        # Get the batch data
+        batch_x = x[i: min(i+batch_size, n_x)]
+        if y is not None:
+            batch_y = y[i: min(i+batch_size, n_x)]
+
+        if enable_shift:
+            # horizontal shift by a random number shift
+            # We have a slightly higher chance for no shift. See if you can understand why.
+            shift = np.random.randint(-n_w, n_w, 1)
+            batch_x = np.roll(batch_x, shift=shift, axis=2)
+
+            # vertical shift
+            shift = np.random.randint(-n_w, n_w, 1)
+            batch_x = np.roll(batch_x, shift=shift, axis=1)
+
+        # Handle batch increment/end
+        i+=batch_size
+        if i>=n_x:
+            i=0
+
+        # Yield data
+        if y is not None:
+            yield( batch_x, batch_y )
+        else:
+            yield( batch_x)
+
---
+++ precision_recall_helper_keras.py
@@ -0,0 +1,106 @@
+"""
+Helper functions to get precision/recall from Keras. 
+"""
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+
+### Use this class with model.fit_generator() to get precision/recall/f1 scores.
+### We need the whole validation data to calculate precision/recall/f1. When using generators in Keras, the validation data is not readily available in the model. It is available in small batches.
+### So we need a special method to take care of this situation.
+### First, we collect all the validation batches, then calculate the scores. If we calculate the scores for each batch, we get incorrect scores.
+"""
+How to use this method:
+    # Set up metrics
+    gen_metric = GeneratorMetrics(
+        val_image_generator,
+        validation_steps)
+
+    # Train the model
+    model2.fit_generator(train_image_generator,
+                        steps_per_epoch=~~,
+                        epochs=~~,
+                        validation_data=val_image_generator,
+                        validation_steps=~~,
+                        callbacks=[gen_metric]
+                        )
+
+    The image generator can be any general purpose image generator or use image_generator_ysn(). 
+"""
+class GeneratorMetrics(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self._data = []
+
+    def get_data(self):
+        return self._data
+
+    def __init__(self, validation_generator, validation_steps):
+        self.validation_generator = validation_generator
+        self.validation_steps = int(validation_steps)
+    
+    def on_epoch_end(self, batch, logs={}):
+        # hold all the validation data
+        y_val_all = []
+        y_predict_all = []
+
+        # loop through validation data in batches and update y_val_all and y_predict_all
+        for batch_index in range(self.validation_steps):
+            X_val, y_val = next(self.validation_generator)            
+            y_predict = np.asarray(self.model.predict(X_val))
+
+            # convert from one_hot to regular notation
+            y_val = np.argmax(y_val, axis=1)
+            y_predict = np.argmax(y_predict, axis=1)
+            
+            y_val_all = y_val_all + list(y_val)
+            y_predict_all = y_predict_all + list(y_predict)
+        
+        # Now all the validation data is collected, let's calculate recall/precision/f1 scores. 
+        self._data.append({
+            'val_recall': recall_score(y_val_all, y_predict_all, average=None),
+            'val_precision': precision_score(y_val_all, y_predict_all, average=None),
+            'f1_score': f1_score(y_val_all, y_predict_all, average=None)
+        })
+        return
+
+### Use this class with model.fit() to get precision/recall scores.
+### This is much simpler because we dont use generators. So all the validation data is available to us.
+class Metrics(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self._data = []
+
+    def on_epoch_end(self, batch, logs={}):
+        # Collect validation data
+        X_val, y_val = self.validation_data[0], self.validation_data[1]
+        y_predict = np.asarray(self.model.predict(X_val))
+
+        # convert from one_hot to regular notation
+        y_val = np.argmax(y_val, axis=1)
+        y_predict = np.argmax(y_predict, axis=1)
+
+        # Calculate the metrics and return them
+        self._data.append({
+            'val_recall': recall_score(y_val, y_predict, average=None),
+            'val_precision': precision_score(y_val, y_predict, average=None),
+            'f1_score': f1_score(y_val, y_predict, average=None)
+        })
+        return
+
+    def get_data(self):
+        return self._data
+
+# Convert the precision/recall array from above to a dataframe with class numbers and epoch numbers
+# Special use method to convert precision/recall/f1 data to a pandas datafram. 
+# I will complete the documentation later. 
+def pr_keras_to_df(pr, n_classes, init_epoch):
+    pr_df = pd.DataFrame()
+    i = init_epoch
+    for prx in pr:
+        prx.update({'epoch':i})
+        prx.update({'class':np.arange(n_classes)})
+        pr_df = pr_df.append(pd.DataFrame(prx))
+        i+=1
+    if le is not None:
+        pr_df['class_desc'] = le.inverse_transform(pr_df['class'])
+    return pr_df
+
+
